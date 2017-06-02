4a2f6bc900bdc3870b4753073ce599e71cc95b4d
silburt <silburt@astro.utoronto.ca>
2017-06-02 16:05:25 -0400

    job submission edit

--- submit_jobs.py
+++ submit_jobs.py
@@ -16,18 +16,8 @@ files = glob.glob('%s/*'%jobs_dir)
 Njobs_counter = 0
 for f in files:
     job_name = f.split(jobs_dir)[1]
-    
-    #if integration has already been run, check if was still stable
-    csv = 'output/%s.csv'%job_name
-    if os.path.exists(csv):
-        time, dE, stable, a1, e1, l1, w1, M1, a2, e2, l2, w2, M2, phi1, phi2, phi3 = np.loadtxt(open(csv, 'r'), delimiter=',', unpack=True)
-        if stable[-1] == 1 and time[-1]<6.26e9:
-            print 'restarting job: %s'%job_name
-            submit_job(f, job_name)
-            Njobs_counter += 1
-    else:
-        submit_job(f, job_name)     #submitting job for the first time
-        Njobs_counter += 1
+    submit_job(f, job_name)     #submitting job for the first time
+    Njobs_counter += 1
 
     if Njobs_counter >= 635:        #640 single-node job limit for sunnyvale
         break
